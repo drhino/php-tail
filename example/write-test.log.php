f7fee69a3142a004ad5293632ce925abc9b54a16
Gerd <gerdachternaam@gmail.com>
2023-04-14 21:19:04 +0000

    adds truncate function

--- example/write-test.log.php
+++ example/write-test.log.php
@@ -8,6 +8,98 @@ $log = __DIR__ . '/test.log';
 clearstatcache(true, $log);
 file_put_contents($log, '');
 
+/**
+ * Appends the $data to the file.
+ * @param string $path to file.
+ * @param string $data to append.
+ * @return void
+ */
+function append(string $path, string $data): void {
+    $x = fopen($path, 'ab');
+    flock($x, LOCK_EX);
+    fwrite($x, $data);
+    fclose($x);
+}
+
+/**
+ * Truncates the number of $bytes from the beginning.
+ * @param string $path to file.
+ * @param integer $bytes to remove.
+ * @param string $delimiter to expect between chunks of data.
+ * @throws Exception
+ * @return integer $written number of bytes.
+ */
+function truncate(string $path, int $bytes, string $delimiter = null): int {
+    // First, we write the last part of the file to the beginning
+    // After, we truncate that file to the number of bytes written
+    try {
+        $write = fopen($path, 'cb');
+        $read = fopen($path, 'rb');
+
+        // Acquires the lock
+        flock($write, LOCK_EX);
+
+        if (isset($delimiter)) {
+            // We start the search for the $delimiter at
+            // 1 character before the requested position
+            // This ensures the exact position matches
+            fseek($read, $bytes - 1);
+
+            // ... @TODO: maximum number of buffers to read?
+            // ... so we wouldn't be reading the whole file
+            // ... if the delimiter does not exist in the file
+            while (!feof($read)) {
+                if (($buffer = fread($read, 8192)) === false) {
+                    throw new Exception('Failed to fread()');
+                }
+
+                if (($index = strpos($buffer, $delimiter)) !== false) {
+                    $bytes = ftell($read) - $index;
+                    break;
+                }
+            }
+        }
+
+        // Sets the file pointer to the number of bytes to remove
+        fseek($read, $bytes);
+
+        // The expected number of bytes to be written
+        $expected = filesize($path) - $bytes;
+        $written = 0;
+
+        // Writes the contents, starting from $bytes, to the beginning of the file
+        while (!feof($read)) {
+            if (($buffer = fread($read, 8192)) === false) {
+                throw new Exception('Failed to fread()');
+            }
+
+            fwrite($write, $buffer);
+            $written += strlen($buffer);
+        }
+
+        if ($expected !== $written) {
+            throw new Exception("Wrote: $written bytes of $expected bytes");
+        }
+
+        // We duplicated the contents to the beginning, now we remove
+        // the original contents, which will shrink the file size
+        ftruncate($write, $written);
+    }
+    catch (Throwable $e) {}
+
+    // Closes the file handle, optional, but best practice
+    is_resource($read) && fclose($read);
+
+    // Release the lock and close the file handle
+    is_resource($write) && fclose($write);
+
+    if (isset($e)) {
+        throw $e;
+    }
+
+    return $written;
+}
+
 $i = 0;
 
 while (connection_status() === CONNECTION_NORMAL) {
@@ -16,24 +108,8 @@ while (connection_status() === CONNECTION_NORMAL) {
     clearstatcache(true, $log);
 
     if (filesize($log) === 1500) {
-        $write = fopen($log, 'cb');
-        flock($write, LOCK_EX);
-        $read = fopen($log, 'rb');
-
-        // The last half of the file
-        fseek($read, 750);
-
-        if (!feof($read)) {
-            if (($buffer = fread($read, 8192)) === false) {
-                die('Failed to fread()');
-            }
-
-            fwrite($write, $buffer);
-        }
-
-        fclose($read);
-        ftruncate($write, 750);
-        fclose($write);
+        // Keeps the last half of the file
+        truncate($log, 750);
     }
 
     // Single character
@@ -41,10 +117,8 @@ while (connection_status() === CONNECTION_NORMAL) {
 		$i = 1;
 	}
 
-    // Length of 15 bytes including newline
-    $append = time() . '---' . $i . PHP_EOL;
-
-    file_put_contents($log, $append, FILE_APPEND);
+    // Appends 15 bytes
+    append($log, time() . '---' . $i . PHP_EOL);
 
     usleep(100000); // 100 ms
 }
