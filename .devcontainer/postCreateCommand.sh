89e70c3f886944373bcd986bac6d07a1fbfaaae7
Gerd <gerdachternaam@gmail.com>
2023-04-14 12:58:44 +0000

    improve codespace and switch to php8.0

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+composer install
+
+# chmod a+x /workspaces/php-tail/example
+sudo rm -rf /var/www/html
+sudo ln -s /workspaces/php-tail/example /var/www/html
+
+sudo apache2ctl start
+
